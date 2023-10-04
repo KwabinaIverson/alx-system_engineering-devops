54e306baf6b320b170b9bd9a0cfb10f2f8024df6
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-10-03 18:23:13 -0700

    Task 0

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,19 +1,2 @@
 #!/usr/bin/env ruby
-
-# Check if the script has received an argument
-if ARGV.length != 1
-  puts "Usage: ./0-simply_match_school.rb <text>"
-  exit 1
-end
-
-# Define the regular expression pattern
-pattern = /School/
-
-# Get the input text from the command line argument
-text = ARGV[0]
-
-# Use the regular expression to match the text
-match = text.match(pattern)
-
-# Output the matched text or an empty string if no match was found
-puts match ? match[0] : ''
+puts ARGV[0].scan(/School/).join
