fad3d8264486ab5cf50540afd21f02903c2056bf
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-10-03 17:58:07 -0700

    Repetition

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,20 +1,2 @@
 #!/usr/bin/env ruby
-
-# Check if the script has received an argument
-if ARGV.length != 1
-  puts "Usage: ./0-simply_match_school.rb <text>"
-  exit 1
-end
-
-# Define the regular expression pattern
-pattern = /\w+t{2,5}+n/
-
-# Get the input text from the command line argument
-text = ARGV[0]
-
-# Use the regular expression to check for a match
-if text =~ pattern
-  puts "Matched: #{$&}" # Output the matched text
-else
-  puts "No match found"
-end
+puts ARGS[0].scan(/htn|hbtn/).join
