c9524aa90ca32a09dbb87117f7e783ee43c61100
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-11-24 07:13:01 -0800

    Task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,4 @@
-# 1-install_a_package.pp
+# init.pp
 
 package { 'python3-pip':
   ensure => installed,
@@ -9,12 +9,4 @@ exec { 'install_flask':
   path        => ['/usr/bin'],
   refreshonly => true,
   require     => Package['python3-pip'],
-}
-
-exec { 'check_flask_version':
-  command => '/usr/bin/flask --version',
-  path    => ['/usr/bin'],
-  logoutput => true,
-  unless  => '/usr/bin/flask --version | grep "Flask 2.1.0"',
-  require => Exec['install_flask'],
 }
